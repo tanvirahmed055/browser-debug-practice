2a7979571f7dc0ce6f9ec1ae6856c18a7bdb2d2a
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-16 13:18:23 +0600

    task-14

---
+++ task-12.js
@@ -0,0 +1,9 @@
+// ১২. কনসোল এ গিয়ে এমন কিছু একটা করো যাতে কোন একটা ফাংশন কে কল করলে কোন কোন প্যারামিটার দিয়ে কল করা হয়েছে সেটা কনসোল করে ফেলবে
+
+//salary-app-debug
+
+//Go to source tab. Press Esc to open console. 
+
+// > monitor(addRecord)
+// < undefined
+// VM154:1 function addRecord called with arguments: Fuad, 12000
