211ce4969aea9bedce06a168423b903068e3ed63
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-16 13:29:08 +0600

    v13

--- task-12.js
+++ task-12.js
@@ -1,5 +1,8 @@
 // ১২. কনসোল এ গিয়ে এমন কিছু একটা করো যাতে কোন একটা ফাংশন কে কল করলে কোন কোন প্যারামিটার দিয়ে কল করা হয়েছে সেটা কনসোল করে ফেলবে
 
+//https://developer.chrome.com/docs/devtools/console/utilities/
+
+
 //salary-app-debug
 
 //Go to source tab. Press Esc to open console. 
---
+++ task-13.js
@@ -0,0 +1,14 @@
+// ১৩. গুগলে সার্চ কনসোল এর ডকুমেন্টেশন খুঁজে বের করো। তারপর এমন কিছু একটা করো। যাতে তুমি একটা ফর লুপ ১ মিলিয়ন বার চালানোর পর, পুরা কোড চলতে কত সময় লাগছে সেটা বের করে ফেলতে পারবে
+
+// https://developer.chrome.com/docs/devtools/console/api/
+
+//Go to Browser Console
+
+// console.time();
+// for (var i = 0; i < 1000000; i++) {
+//   let square = i ** 2;
+// }
+// console.timeEnd();
+
+
+// default: 119.762939453125 ms
