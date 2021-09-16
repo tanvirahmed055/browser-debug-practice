9c02d9d1bfa20420ad0bf85fdaba1d6082f0763a
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-16 14:14:48 +0600

    v4

---
+++ task-4.js
@@ -0,0 +1,29 @@
+// ৫. একটা url এর মধ্যে কী কী পার্টস থাকে
+
+
+// developer.mozilla.org
+
+// here developer is called subdomain. If anything is with a dot before .com, .org etc. then it's called subdomain.
+
+//  mozilla.org part is domain here
+
+
+
+
+//   https://example.org:8080/foo/bar?q=baz#bang
+
+// full url is called href
+
+// https: is protocol
+
+//  example.org is hostname or you can say domain name. No subdomain can be seen here.
+
+//  8080 is port
+
+// https://example.org:8080  you can this part origin
+
+// foo/bar is pathname
+
+// q=baz is search
+
+// #bang is hash
