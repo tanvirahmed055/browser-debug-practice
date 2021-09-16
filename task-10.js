ba5b168e20c11608867663e75891513e24671659
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-16 14:30:56 +0600

    v10

---
+++ task-10.js
@@ -0,0 +1,12 @@
+// ১০. element ট্যাবে কোন একটা class এর নাম দিয়ে element খুঁজে বের করো
+
+
+// Go to element tab then press ctrl+ F. Then you will find something like a searchbox to type in the below.
+
+// Suppose I want to find button inside modal-dialog class then I will type this in the searchbox.
+
+
+//    .modal-dialog button
+
+// Just type and search like CSS selector
+
